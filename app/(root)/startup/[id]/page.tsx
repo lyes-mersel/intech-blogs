9fa414feaa6f05be4d2e761c80e659e40b01246d
Lyes MERSEL <dev.mersel@gmail.com>
2024-11-06 19:35:30 +0100

    last commit before deploying tests

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -20,13 +20,13 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
   if (!post) return notFound();
-
-  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-    slug: "editor-picks",
-  });
-  console.log(editorPosts);
 
   const parsedContent = md.render(post?.pitch || "");
 
