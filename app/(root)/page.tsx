48769cb85769fa11c3e8a46facf1d6e55c25a327
Lyes MERSEL <dev.mersel@gmail.com>
2024-11-06 03:33:14 +0100

    Add Startups Collections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,39 +5,41 @@ import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
-   searchParams,
+  searchParams,
 }: {
-   searchParams: Promise<{ query?: string }>;
+  searchParams: Promise<{ query?: string }>;
 }) {
-   const query = (await searchParams).query;
-   const params = { search: query || null };
-   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-   return (
-      <main className={"font-work-sans"}>
-         <section className="pink_container">
-            <h1 className={"heading"}>
-               Pitch Your Startup, <br /> Connect with Entrepreneurs
-            </h1>
-            <p className="sub-heading !max-w-3xl">
-               Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
-            </p>
-            <SearchForm query={query} />
-         </section>
-         <section className="section_container">
-            <p className="text-30-semibold">
-               {query ? `Search results for "${query}"` : "All Startups"}
-            </p>
-            <ul className="mt-7 card_grid">
-               {posts.length > 0 ? (
-                  posts.map((post: StartupCardType) => <StartupCard key={post._id} post={post} />)
-               ) : (
-                  <p>No startups found</p>
-               )}
-            </ul>
-         </section>
+  return (
+    <main className={"font-work-sans"}>
+      <section className="pink_container">
+        <h1 className={"heading"}>
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p>No startups found</p>
+          )}
+        </ul>
+      </section>
 
-         <SanityLive />
-      </main>
-   );
+      <SanityLive />
+    </main>
+  );
 }
