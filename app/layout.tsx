0a4e2cc0ac7e267adc7aed6d5f4ec6b4e5e77c31
Lyes MERSEL <dev.mersel@gmail.com>
2024-11-12 03:40:08 +0100

    Fix the Favicons Problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
 import { Toaster } from "@/components/ui/toaster";
-import Head from "next/head";
 
 const workSans = localFont({
   src: [
@@ -47,28 +46,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <body className={workSans.variable}>
         {children}
         <Toaster />
